ba10674babf05ad531c70f153c10ece19bc8b726
Chris Fisher <chrisfisher@Chriss-MacBook-Air.local>
2022-10-18 22:00:51 -0700

    Table

--- golf_app/lib/main.dart
+++ golf_app/lib/main.dart
@@ -1,7 +1,182 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(InsertDataTable());
+  //runApp(const MyApp());
+}
+
+class Game {
+  int score;
+  String course;
+  int par;
+  Game(this.score, this.course, this.par);
+}
+
+class InsertDataTable extends StatefulWidget {
+  @override
+  _InsertDataTableState createState() => new _InsertDataTableState();
+}
+
+class _InsertDataTableState extends State<InsertDataTable> {
+  List<Game> ScoreLst = <Game>[
+    //Game(82, "Bidwell Golf Course"),
+    //Game(79, "Los Lagos Golf Course"),
+  ];
+
+  final formKey = new GlobalKey<FormState>();
+  var Score_Controller = new TextEditingController();
+  var Course_Controller = new TextEditingController();
+  var Par_Controller = new TextEditingController();
+  //var lastID = 2;
+  var total = 0;
+  double fin = 0;
+  var c = 0;
+
+  @override
+  // Method that call only once to initiate the default app.
+  void initState() {
+    super.initState();
+    //lastID++;
+    //ID_Controller.text = lastID.toString();
+  }
+
+  // Method that is used to refresh the UI and show the new inserted data.
+  refreshList(int s, int p) {
+    setState(() {
+      total = total + s - p;
+      c++;
+      fin = total / c;
+      //ID_Controller.text = lastID.toString();
+    });
+  }
+
+  // Method used to validate the user data
+  validate() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      String Score = Score_Controller.text;
+      String Course = Course_Controller.text;
+      String Par = Par_Controller.text;
+      Game s = Game(int.parse(Score), Course, int.parse(Par));
+      ScoreLst.add(s);
+      //lastID++;
+      refreshList(int.parse(Score), int.parse(Par));
+      Score_Controller.text = "";
+      Course_Controller.text = "";
+      Par_Controller.text = "";
+    }
+  }
+
+  // Method to check the value of age, age is int or not
+  bool NotIntCheck(var N) {
+    final V = int.tryParse(N);
+
+    if (V == null) {
+      print("Not Int");
+      return true;
+    } else {
+      print("Int");
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MaterialApp with home as scaffold
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Handicap: $fin',
+          ), //var age; print('Age is: $age');
+        ),
+        body: ListView(
+          children: <Widget>[
+            Form(
+              key: formKey,
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Score:"),
+                    TextFormField(
+                      controller: Score_Controller,
+                      keyboardType: TextInputType.number,
+                      validator: (val) => NotIntCheck(val)
+                          ? 'Enter Score,Number Required'
+                          : null,
+                    ),
+                    Text("Course Name:"),
+                    TextFormField(
+                      controller: Course_Controller,
+                      keyboardType: TextInputType.text,
+                      validator: (val) =>
+                          val?.length == 0 ? 'Enter Course Name' : null,
+                    ),
+                    Text("Par:"),
+                    TextFormField(
+                      controller: Par_Controller,
+                      keyboardType: TextInputType.number,
+                      validator: (val) =>
+                          NotIntCheck(val) ? 'Enter Par,Number Required' : null,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: MaterialButton(
+                        color: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Text(
+                          'Insert Score',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: validate,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: [
+                  DataColumn(
+                    label: Text("Score"),
+                  ),
+                  DataColumn(
+                    label: Text("Course Name"),
+                  ),
+                  DataColumn(
+                    label: Text("Par"),
+                  ),
+                ],
+                rows: ScoreLst.map(
+                  (s) => DataRow(cells: [
+                    DataCell(
+                      Text(s.score.toString()),
+                    ),
+                    DataCell(
+                      Text(s.course),
+                    ),
+                    DataCell(
+                      Text(s.par.toString()),
+                    ),
+                  ]),
+                ).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +186,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Handicap: ',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +197,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Golf Calculator'),
     );
   }
 }
